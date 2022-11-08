c11f11c8d6ba31fd495331d0b7d5133a516eb56e
Joao <joaovitorprestesgarcia@gmail.com>
2022-11-08 16:34:12 -0300

    add database recording

--- src/composable/userPhotoGallery.ts
+++ src/composable/userPhotoGallery.ts
@@ -1,50 +1,72 @@
 import { ref, onMounted, watch } from "vue";
-import { Camera, CameraResultType, CameraSource, Photo } from "@capacitor/camera";
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
 import { Filesystem, Directory } from "@capacitor/filesystem";
 import { Preferences } from "@capacitor/preferences";
 
 export interface UserPhoto {
-    filepath: string;
-    webviewPath?: string;
+  filepath: string;
+  webviewPath?: string;
 }
 
-
-
-
 export function userPhotoGallery() {
-    const photos = ref<UserPhoto[]>([]);
+  const PHOTO_STORAGE = 'photos';
+  const photos = ref<UserPhoto[]>([]);
 
-    const convertBlobTobase64 = (blob: Blob) =>
-      new Promise ((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onerror = reject;
-        reader.onload = () => {
-          resolve(reader.result);
-        };
-        reader.readAsDataURL(blob);
+  const cachePhotos = () => {
+    Preferences.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(photos.value), //JSO,parse()
+    });
+  };
+
+  const convertBlobTobase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+
+    const savePicture = async (photo: Photo, fileName: string):
+    Promise<UserPhoto> => {
+     // let base64Data: string;
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      const base64Data = (await convertBlobTobase64(blob)) as string;
+      const savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: Directory.Data,
       });
-
-    const takePhoto = async () => {
-      const photo = await Camera.getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 80,
-      });
-
-      const fileName = new Date().getTime() + '.jpeg';
-      const savedFileImage = {
+      return {
         filepath: fileName,
         webviewPath: photo.webPath,
       };
-
-      photos.value = [savedFileImage, ...photos.value];
-
     };
 
-    return {
-        photos,
-        takePhoto,
-    };
- }
+  const takePhoto = async () => {
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 80,
+    });
 
+    const fileName = new Date().getTime() + ".jpeg";
+    const savedFileImage = await savePicture(photo, fileName); 
+    
+    photos.value = [savedFileImage, ...photos.value];
+  };
 
+  return {
+    photos,
+    takePhoto,
+  };
+}
