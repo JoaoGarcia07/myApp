5a31487e74a348d72a3db79fa87490e4f4bb8216
Joao <joaovitorprestesgarcia@gmail.com>
2022-11-22 16:58:35 -0300

    add android

--- src/composable/userPhotoGallery.ts
+++ src/composable/userPhotoGallery.ts
@@ -7,6 +7,8 @@ import {
 } from "@capacitor/camera";
 import { Filesystem, Directory } from "@capacitor/filesystem";
 import { Preferences } from "@capacitor/preferences";
+import { isPlatform } from "@ionic/vue";
+import { Capacitor } from "@capacitor/core";
 
 export interface UserPhoto {
   filepath: string;
@@ -14,7 +16,7 @@ export interface UserPhoto {
 }
 
 export function userPhotoGallery() {
-  const PHOTO_STORAGE = 'photos';
+  const PHOTO_STORAGE = "photos";
   const photos = ref<UserPhoto[]>([]);
 
   const cachePhotos = () => {
@@ -34,23 +36,38 @@ export function userPhotoGallery() {
       reader.readAsDataURL(blob);
     });
 
-
-    const savePicture = async (photo: Photo, fileName: string):
-    Promise<UserPhoto> => {
-     // let base64Data: string;
-      const response = await fetch(photo.webPath!);
-      const blob = await response.blob();
-      const base64Data = (await convertBlobTobase64(blob)) as string;
-      const savedFile = await Filesystem.writeFile({
-        path: fileName,
-        data: base64Data,
-        directory: Directory.Data,
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    let base64Data: string;
+    if (isPlatform("hybrid")) {
+      const file = await Filesystem.readFile({
+        path: photo.path!,
       });
+      base64Data = file.data;
+    }
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    base64Data = (await convertBlobTobase64(blob)) as string;
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    if (isPlatform("hybrid")) {
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
       return {
         filepath: fileName,
         webviewPath: photo.webPath,
       };
-    };
+    }
+  };
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -60,11 +77,32 @@ export function userPhotoGallery() {
     });
 
     const fileName = new Date().getTime() + ".jpeg";
-    const savedFileImage = await savePicture(photo, fileName); 
-    
+    const savedFileImage = await savePicture(photo, fileName);
+
     photos.value = [savedFileImage, ...photos.value];
   };
 
+  const loadSaved = async () => {
+    const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+    const photosInPreferences = photoList.value
+      ? JSON.parse(photoList.value)
+      : [];
+
+      if (!isPlatform('hybrid')){
+    for (const photo of photosInPreferences) {
+      const file = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+      photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+    }
+  }
+
+    photos.value = photosInPreferences;
+  };
+
+  watch(photos, cachePhotos);
+  onMounted(loadSaved);
   return {
     photos,
     takePhoto,
